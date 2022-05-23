0a5e46084dbc64ba38a6475cf71620427aec9bb2
Andy Gorbould <gorbould@mail.com>
2022-05-23 15:50:11 +0200

    I can now get one of the 'moves' printed to HTML!

--- script.js
+++ script.js
@@ -25,37 +25,116 @@
 
 // THIS APPEARS TO WORK!
 // https://www.youtube.com/watch?v=XL68br6JyYs
-const nameDisplay = document.getElementById("nameDisplay");
-const pokemonNumb = 20;
+// const nameDisplay = document.getElementById("nameDisplay");
+// const pokemonNumb = 20;
 
 
-const fetchPokemon = async () => {
-    let i = 598
-        await getPokemon(i);
+// const fetchPokemon = async () => {
+//     let i = 598
+//         await getPokemon(i);
 
     
+// };
+
+// const getPokemon = async id => {
+//     const url =
+//     `https://pokeapi.co/api/v2/pokemon/${id}`;
+//     const result = await fetch(url);
+//     const pokemon = await result.json();
+//     createCard(pokemon.name);
+// };
+
+// fetchPokemon();
+
+// function createCard(pokemon) {
+//     const pokemonElement = document.createElement('div');
+//     pokemonElement.classList.add('pokemon');
+
+
+//     // const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+//     const pokemonInnerHTML = `<h3> ${pokemon} </h3>`;
+
+//     pokemonElement.innerHTML = pokemonInnerHTML;
+
+//     nameDisplay.appendChild(pokemonElement);
+// };
+
+
+
+
+const poke_container = document.getElementById('nameDisplay');
+const pokemons_number = 20;
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+const main_types = Object.keys(colors);
+// const main_moves = Object.keys(colors);
+
+const fetchPokemons = async () => {
+	// for (let i = 1; i <= pokemons_number; i++) {
+	// 	await getPokemon(i);
+	// }
+    
+	let i = 101; {
+		await getPokemon(i);
+	}
 };
 
 const getPokemon = async id => {
-    const url =
-    `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const result = await fetch(url);
-    const pokemon = await result.json();
-    createCard(pokemon.name);
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const res = await fetch(url);
+	const pokemon = await res.json();
+	createPokemonCard(pokemon);
 };
 
-fetchPokemon();
+function createPokemonCard(pokemon) {
+	const pokemonEl = document.createElement('div');
+	pokemonEl.classList.add('pokemon');
 
-function createCard(pokemon) {
-    const pokemonElement = document.createElement('div');
-    pokemonElement.classList.add('pokemon');
+// moves -- what a long-winded approach!! this needs shortening....
+    const move = pokemon.moves[1].move.name[0].toUpperCase() + pokemon.moves[1].move.name.slice(1);
 
+// types
+    const poke_types = pokemon.types.map(type => type.type.name);
+	const type = main_types.find(type => poke_types.indexOf(type) > -1);
+    const color = colors[type];
 
-    // const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+// name
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
-    const pokemonInnerHTML = `<h3> ${pokemon} </h3>`;
+	
+	pokemonEl.style.backgroundColor = color;
 
-    pokemonElement.innerHTML = pokemonInnerHTML;
+	const pokeInnerHTML = `
+        
+        <div class="info">
+            <span class="number">#${pokemon.id
+							.toString()
+							.padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">MOVES MOVES MOVES: <span>${move}</span></br></small>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>
+    `;
 
-    nameDisplay.appendChild(pokemonElement);
-};
+	pokemonEl.innerHTML = pokeInnerHTML;
+
+	poke_container.appendChild(pokemonEl);
+}
+
+fetchPokemons();
+
