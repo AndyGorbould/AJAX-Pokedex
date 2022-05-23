be6aeba537c602648347d67a522370591a4fedf2
Andy Gorbould <gorbould@mail.com>
2022-05-23 16:14:34 +0200

    more comments added to script

--- script.js
+++ script.js
@@ -33,7 +33,7 @@
 //     let i = 598
 //         await getPokemon(i);
 
-    
+
 // };
 
 // const getPokemon = async id => {
@@ -66,74 +66,91 @@
 const poke_container = document.getElementById('nameDisplay');
 const pokemons_number = 20;
 const colors = {
-	fire: '#FDDFDF',
-	grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
 };
+
+// remove color key
 const main_types = Object.keys(colors);
 // const main_moves = Object.keys(colors);
 
 const fetchPokemons = async () => {
-	// for (let i = 1; i <= pokemons_number; i++) {
-	// 	await getPokemon(i);
-	// }
-    
-	let i = 101; {
-		await getPokemon(i);
-	}
+    // for (let i = 1; i <= pokemons_number; i++) {
+    // 	await getPokemon(i);
+    // }
+
+
+    // this needs to be user-input
+    let i = 199; {
+        await getPokemon(i);
+    }
 };
 
 const getPokemon = async id => {
-	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-	const res = await fetch(url);
-	const pokemon = await res.json();
-	createPokemonCard(pokemon);
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    createPokemonCard(pokemon);
 };
 
 function createPokemonCard(pokemon) {
-	const pokemonEl = document.createElement('div');
-	pokemonEl.classList.add('pokemon');
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
 
-// moves -- what a long-winded approach!! this needs shortening....
-    const move = pokemon.moves[1].move.name[0].toUpperCase() + pokemon.moves[1].move.name.slice(1);
+    // moves -- what a long-winded approach!! this needs shortening.... forEach move in moves??
+    // const move = pokemon.moves[0].move.name[0].toUpperCase() + pokemon.moves[0].move.name.slice(1);
+    const moveA = pokemon.moves[0].move.name;
+    const moveB = pokemon.moves[1].move.name;
+    const moveC = pokemon.moves[2].move.name;
+    const moveD = pokemon.moves[3].move.name;
 
-// types
+    // types
     const poke_types = pokemon.types.map(type => type.type.name);
-	const type = main_types.find(type => poke_types.indexOf(type) > -1);
+    const type = main_types.find(type => poke_types.indexOf(type) > -1);
     const color = colors[type];
 
-// name
-	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    // name
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
-	
-	pokemonEl.style.backgroundColor = color;
 
-	const pokeInnerHTML = `
+    pokemonEl.style.backgroundColor = color;
+
+    // this innerHTML thing needs to change - create element/li for each move
+    const pokeInnerHTML = `
         
         <div class="info">
             <span class="number">#${pokemon.id
-							.toString()
-							.padStart(3, '0')}</span>
+            .toString()
+            .padStart(3, '0')}</span>
             <h3 class="name">${name}</h3>
-            <small class="type">MOVES MOVES MOVES: <span>${move}</span></br></small>
+            
+            <h2>this looks terrible & over-complicated!</h2>
+            <small class="type">Moves: <span>${moveA}</span></br></small>
+            <small class="type">Moves: <span>${moveB}</span></br></small>
+            <small class="type">Moves: <span>${moveC}</span></br></small>
+            <small class="type">Moves: <span>${moveD}</span></br></small>
+
+
+            </br>
             <small class="type">Type: <span>${type}</span></small>
         </div>
     `;
 
-	pokemonEl.innerHTML = pokeInnerHTML;
+    pokemonEl.innerHTML = pokeInnerHTML;
 
-	poke_container.appendChild(pokemonEl);
+    poke_container.appendChild(pokemonEl);
 }
 
 fetchPokemons();
