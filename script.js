e5dfd0d99419e8795485bca9f8b72c83b38a7e1c
Andy Gorbould <gorbould@mail.com>
2022-05-24 16:50:59 +0200

    working but evolution fetch fetch fetch messed up

--- script.js
+++ script.js
@@ -1,78 +1,6 @@
-//define API URL
-// let api = "https://pokeapi.co/api/v2/";
-
-// console.log(api);
-
-
-// // AJAX w3
-// function loadDoc() {
-//     const xhttp = new XMLHttpRequest();
-//     xhttp.onload = function() {
-//       document.getElementById("nameDisplay").innerHTML = this.responseText;
-//       }
-//     xhttp.open("GET", "https://pokeapi.co/api/v2/?limit=1&offset=1", true);
-//     xhttp.send();
-//   }
-
-
-
-// // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-// fetch("https://pokeapi.co/api/v2/pokemon/")
-//     .then(response => response.json())
-//     .then(data => console.log(data.results[2].name));
-//     document.getElementById("nameDisplay").innerHTML = (data.results[2].name);
-
-
-// THIS APPEARS TO WORK!
-// https://www.youtube.com/watch?v=XL68br6JyYs
-// const nameDisplay = document.getElementById("nameDisplay");
-// const pokemonNumb = 20;
-
-
-// const fetchPokemon = async () => {
-//     let i = 598
-//         await getPokemon(i);
-
-
-// };
-
-// const getPokemon = async id => {
-//     const url =
-//     `https://pokeapi.co/api/v2/pokemon/${id}`;
-//     const result = await fetch(url);
-//     const pokemon = await result.json();
-//     createCard(pokemon.name);
-// };
-
-// fetchPokemon();
-
-// function createCard(pokemon) {
-//     const pokemonElement = document.createElement('div');
-//     pokemonElement.classList.add('pokemon');
-
-
-//     // const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-
-//     const pokemonInnerHTML = `<h3> ${pokemon} </h3>`;
-
-//     pokemonElement.innerHTML = pokemonInnerHTML;
-
-//     nameDisplay.appendChild(pokemonElement);
-// };
-
-
-
-
-
-
-
 // input
 // button
 var run = document.getElementById("btnNumbInput").addEventListener("click", userNumberInputFunc);
-
-
-// init "i"
-// let i = 130;
 
 function userNumberInputFunc() {
 
@@ -111,10 +39,6 @@ const main_types = Object.keys(colors);
 // const main_moves = Object.keys(colors);
 
 const fetchPokemons = async () => {
-    // for (let i = 1; i <= pokemons_number; i++) {
-    // 	await getPokemon(i);
-    // }
-
     {
         await getPokemon(i);
     }
@@ -125,18 +49,35 @@ const getPokemon = async id => {
     const res = await fetch(url);
     const pokemon = await res.json();
     createPokemonCard(pokemon);
+    // species url
+    const speciesURL = (pokemon.species.url);       // https://pokeapi.co/api/v2/pokemon-species/4/
+    const reultSpecies = await fetch(speciesURL);
+    const speciesTree = await reultSpecies.json();
+    // evo chain
+    const evoChainURL = (speciesTree.evolution_chain.url);    // https://pokeapi.co/api/v2/evolution-chain/2/
+    const resultEvoChain = await fetch(evoChainURL);
+    const evoChainEnd = await resultEvoChain.json();
+    const helpMePlease = evoChainEnd.chain.evolves_to;
+    const helpMePleaseEvTo = helpMePlease.evolves_to.species;
+    // const helpMePleaseEvToSpecies = helpMePleaseEvTo.name;
+    
+    
+    /// testing
+    // console.log(evoChainURL);
+    console.log(helpMePleaseEvTo);
 };
 
 function createPokemonCard(pokemon) {
     const pokemonEl = document.createElement('div');
     pokemonEl.classList.add('pokemon');
 
+    
+
 
     // img
     const img = pokemon.sprites.front_default;
 
     // moves -- what a long-winded approach!! this needs shortening.... forEach move in moves??
-    // const move = pokemon.moves[0].move.name[0].toUpperCase() + pokemon.moves[0].move.name.slice(1);
     const moveA = pokemon.moves[0].move.name;
     const moveB = pokemon.moves[1].move.name;
     const moveC = pokemon.moves[2].move.name;
@@ -154,25 +95,20 @@ function createPokemonCard(pokemon) {
     pokemonEl.style.backgroundColor = color;
 
     // this innerHTML thing needs to change - create element/li for each move
-    const pokeInnerHTML = `
-        
+    const pokeInnerHTML = `        
         <div class="info">
             <span class="number">#${pokemon.id
             .toString()
             .padStart(3, '0')}</span>
             <h3 class="name">${name}</h3>
-
-
             
             <img class="sprite-front" src="${img}" alt="${name}">
             </br>
-
 
             <small class="type">Moves: <span>${moveA}</span></br></small>
             <small class="type">Moves: <span>${moveB}</span></br></small>
             <small class="type">Moves: <span>${moveC}</span></br></small>
             <small class="type">Moves: <span>${moveD}</span></br></small>
-
 
             </br>
             <small class="type">Type: <span>${type}</span></small>
@@ -182,51 +118,13 @@ function createPokemonCard(pokemon) {
     pokemonEl.innerHTML = pokeInnerHTML;
 
     poke_container.appendChild(pokemonEl);
+
+
+
+
+    
 }
 
 fetchPokemons();
 
 
-
-// 
-// 
-// 
-// list
-// https://pokeapi.co/api/v2/pokemon/?limit=50&offset=50
-
-const poke_list = document.getElementById('list');
-
-
-const fetchList = async () => {
-    await getPokemonList(i);
-};
-
-const getPokemonList = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon/?limit=100&offset=00}`;
-    const res = await fetch(url);
-    const pokemonList = await res.json();
-    createPokemonList(pokemonList);
-};
-
-
-
-
-function createPokemonList(pokemonList) {
-    const pokemonElementList = document.createElement('option');
-    pokemonElementList.classList.add('pokemonList');
-
-
-    // name
-    const name = pokemonList.name[0].toUpperCase() + pokemonList.name.slice(1);
-    // id
-    const pokemonListID = pokemonList.id;
-
-    // this innerHTML thing needs to change - create element/li for each
-    const pokeInnerHTML = `
-                <option value="${pokemonList.id}">${pokemonList.name}</option>
-        `;
-
-    pokemonElementList.innerHTML = pokeInnerHTML;
-
-    poke_list.appendChild(pokemonElementList);
-};
